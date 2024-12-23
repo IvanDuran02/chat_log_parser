82136ddb8d28ffd3aa0f1a4db7605497ad653c04
IvanDuran02 <iamivanduran02@gmail.com>
2024-12-23 02:02:47 -0500

    feat: added total count + individual

---
+++ READEME.md
@@ -0,0 +1,6 @@
+# Todo:
+
+- add to total messages
+    - total messages from both users (what percentage did someone message more than the other)
+
+- 
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,60 @@
 use serde_json::Value;
+use std::collections::HashMap;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Read Json file
-    let file_content = fs::read_to_string("src/chat_logs/discord_messages.json")?;
+    let file_content = fs::read_to_string("src/chat_logs/alec_logs.json")?;
 
     // Parse the JSON into a generic Value
     let json: Value = serde_json::from_str(&file_content)?;
 
+    let mut total_messages = 0;
     let mut total_call_duration = 0;
+    let mut longest_call = 0;
+
+    // HashMap for counting individual messages
+    let mut individual_messages = HashMap::new();
 
     // Access specific fields in JSON
-    if let Some(messages) = json.get("messages") {
-        for message in messages.as_array().unwrap_or(&vec![]) {
-            /* if let Some(author) = message.get("author") {
-                println!("Author: {}", author);
-            } */
+    if let Some(messages) = json.get("messages").and_then(|m| m.as_array()) {
+        for message in messages {
+            // Access the "author" field in the current message
+            if let Some(author) = message.get("author") {
+                // Access the "nickname" field inside "author"
+                if let Some(nickname) = author.get("nickname").and_then(|n| n.as_str()) {
+                    *individual_messages.entry(nickname).or_insert(0) += 1;
+                }
+            }
 
+            // Read messages
             if let Some(content) = message.get("content").and_then(|c| c.as_str()) {
+                total_messages += 1;
                 if let Some(duration) = extract_call_duration(content) {
                     total_call_duration += duration;
+                    if duration > longest_call {
+                        longest_call = duration;
+                    }
                 }
             }
         }
     }
 
-    let hours = total_call_duration / 60;
-    let minutes = total_call_duration - (hours * 60);
-
-    println!("Total Call Time: {} hours and {} minutes.", hours, minutes);
+    println!("Total Messages: {}", total_messages);
+    for (key, value) in &individual_messages {
+        println!("{}: {}", key, value);
+    }
+    println!("Total Call Time: {}", format_time(total_call_duration));
+    println!("Longest Call Was: {}", format_time(longest_call));
 
     Ok(())
+}
+
+fn format_time(duration: i32) -> String {
+    let hours = duration / 60;
+    let minutes = duration % 60;
+
+    format!("{} hours and {} minutes", hours, minutes)
 }
 
 fn extract_call_duration(content: &str) -> Option<i32> {
